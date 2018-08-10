a873aa1fca40e378014d73ce7b3f40c6b7df384b
Mahendra R <mrekapally@credomobile.com>
2018-08-10 14:32:02 -0700

    Change to trigger v2 of lambda package

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -6,6 +6,10 @@ output "lambda_function_name" {
   value = "${aws_lambda_function.checkipaddress.function_name}"
 }
 
+output "lambda_function_version" {
+  value = "${aws_lambda_function.checkipaddress.version}"
+}
+
 output "lambda_function_version_metadata" {
   value = "${data.aws_s3_bucket_object.checkipaddress_pkg.metadata}"
 }
